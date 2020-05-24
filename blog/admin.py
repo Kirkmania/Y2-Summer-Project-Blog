3b55548ec2b68458b7e0c6eaff24893466ebdd53
Kirkmania <george.kirkman27@gmail.com>
2020-05-24 21:37:00 +0100

    added comment functionality

--- blog/admin.py
+++ blog/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
-from .models import Post    #import our post model we made
-
-admin.site.register(Post)   #register our post model
+from .models import Post, Comment    #import our post model we made
 
 # Register your models here.
+admin.site.register(Post)   #register our post model
+admin.site.register(Comment)
