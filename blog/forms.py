6ad17e5fa96340c7717736e95f44bf486000deb9
Kirkmania <george.kirkman27@gmail.com>
2020-08-15 05:58:32 +0100

    TESTING BUGFIX FOR PYTHONANYWHERE NOT FINAL

--- blog/forms.py
+++ blog/forms.py
@@ -12,7 +12,6 @@ class PostForm(forms.ModelForm):
 
         widgets = {
             'title': forms.TextInput(attrs={'class': 'form-control'}),
-            'category': forms.Select(choices=choices, attrs={'class': 'form-control'}),
             'text':  forms.TextInput(attrs={'class': 'form-control'}),
         }
 
