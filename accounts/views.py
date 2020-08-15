bc67fa68f8bfa2653bdfd4f2b1efa13538cb71dc
Kirkmania <george.kirkman27@gmail.com>
2020-08-15 04:35:29 +0100

    styling + extra fields for registration page

--- accounts/views.py
+++ accounts/views.py
@@ -4,21 +4,25 @@ from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import Group
 from django.contrib.auth import login, authenticate
 from django.urls import reverse_lazy
+from .forms import RegisterForm
 
 def signup(request):                                  
     if request.method == "POST":
-        form = UserCreationForm(request.POST)
+        form = RegisterForm(request.POST)
         if form.is_valid():
             form.save()
-            username = form.cleaned_data.get('username')
-            raw_password = form.cleaned_data.get('password1')
-            user = authenticate(username=username, password=raw_password)
 
+            # auto-login
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=password)
+
+            # auto-assign "newcomer" group
             group = Group.objects.get(name='newcomer')
             user.groups.add(group)
 
             login(request, user)
             return redirect('post_list')
     else:
-        form = UserCreationForm()
+        form = RegisterForm()
     return render(request, 'registration/register.html', {'form': form})
